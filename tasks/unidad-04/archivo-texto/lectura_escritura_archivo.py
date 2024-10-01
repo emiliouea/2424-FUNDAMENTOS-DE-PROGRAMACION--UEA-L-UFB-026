7ce69aa7e4e0f02c123c3c2bb7478ea3725a850b
Emilio Senguana <sengua2000@outlook.com>
2024-10-01 16:24:36 -0500

    escritura y lectura de archivo

--- tasks/unidad-04/archivo-texto/lectura_escritura_archivo.py
+++ tasks/unidad-04/archivo-texto/lectura_escritura_archivo.py
@@ -7,6 +7,7 @@ with open("my_notes.txt", "w") as file:
     file.write("Usando metodo 'write' para escribir datos en el archivo.\n")
     file.write("El uso de 'with' facilita la gestión de archivos.\n")
 
+
 # 2. Lectura del archivo de texto
 # Abre el archivo 'my_notes.txt' en modo lectura ('r').
 with open("my_notes.txt", "r") as file:
